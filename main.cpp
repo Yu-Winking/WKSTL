5c540e55dad24803ab40c1f73049062a035f6b87
Winki <Winki@YWK-XPS9310>
2022-06-26 20:47:57 +0800

    添加项目文件。

---
+++ main.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+
+#include "wkSTL.h"
+
+
+#pragma pack(1) 
+struct T
+{
+	uint8_t a:1;
+	uint16_t b:2;
+	uint32_t c:4;
+};
+#pragma pack() 
+
+int main()
+{
+	//int a, b;
+	//std::cin >> a  >> b;
+	//std::cout << a << std::endl;
+	//std::cout << b << std::endl;
+	size_t pushLen(0);
+	wkSTL::Pipeline pipeline;
+
+	std::vector<short> vec{ 1,2,3,4,5,6,7,8,9,10 };
+	pushLen = pipeline.push(vec);
+	std::cout << "push vector length:" << pushLen << std::endl;
+	pipeline.push('\0');
+
+	T tmp{ 0xa, 0xb, 0xc };
+	pushLen = pipeline.push(tmp);
+	std::cout << "push struct length:" << pushLen << std::endl;
+	pipeline.push('\0');
+
+	int in{ 0x12345678 };
+	pushLen = pipeline.push(in);
+	std::cout << "push int length:" << pushLen << std::endl;
+	pipeline.push('\0');
+
+	uint64_t i{ 0xF0F1F2F3F4F5F6F7 };
+	pushLen = pipeline.push(i);
+	std::cout << "push uint64_t length:" << pushLen << std::endl;
+	pipeline.push('\0');
+
+	double d{ 12.334 };
+	pushLen = pipeline.push(d);
+	std::cout << "push double length:" << pushLen << std::endl;
+	pipeline.push('\0');
+
+	long long l{ 0xA1A2A3A4A5A6A7A };
+	pushLen = pipeline.push(l);
+	std::cout << "push long long length:" << pushLen << std::endl;
+	pipeline.push('\0');
+
+	const char c1[] = "0123456789";
+	pushLen = pipeline.push(c1);
+	std::cout << "push char 1 length:" << pushLen << std::endl;
+	pipeline.push('\0');
+
+	const char* c2 = "abcdefghijklmn";
+	pushLen = pipeline.push(c2, static_cast<int>(strlen(c2)+ 1));
+	std::cout << "push char 2 length:" << pushLen << std::endl;
+	pipeline.push('\0');
+
+
+	std::string str{ "我是一位C++爱好者" };
+	pushLen = pipeline.push(str);
+	std::cout << "push string length:" << pushLen << std::endl;
+	pipeline.push('\0');
+
+	std::cout << "pipeline length:" << pipeline.length() << std::endl;
+	std::cout << "pipeline content:" << pipeline << std::endl;
+
+	//uint8_t f{ 0xbb };
+	//std::cout << "f in pos:" << pipeline.at(pipeline.search(f)) << std::endl;
+	std::cout << "s in pos:" << pipeline.at(pipeline.search(str)) << std::endl;
+
+	//char pop[10] = {};
+
+	//size_t popLen = pipeline.pop(pop, 10);
+	//std::cout << popLen << std::endl;
+	//std::cout << pop << std::endl;
+	//std::cout << pipeline.size() << std::endl;
+}
+
---
+++ wkSTL.h
@@ -0,0 +1,258 @@
+#pragma once
+
+#include <queue>
+#include <mutex>
+#include <deque>
+#include <string>
+#include <iomanip>
+
+namespace wkSTL
+{
+	using namespace std;
+
+	template <class T> class Queue
+	{
+	public:
+		Queue(Queue const&) = delete;
+		Queue(Queue&&) = delete;
+		Queue& operator=(Queue const&) = delete;
+		Queue& operator=(Queue&&) = delete;
+
+		Queue()
+			: queue_(), mutex_()
+		{}
+
+		virtual ~Queue()
+		{
+			while (!queue_.empty())
+				queue_.pop();
+		}
+
+		void push(T _record)
+		{
+			lock_guard<mutex> lock(mutex_);
+			queue_.emplace(move(_record));
+		}
+
+		bool pop(T& _record)
+		{
+			lock_guard<mutex> lock(mutex_);
+
+			if (queue_.empty())
+				return false;
+
+			_record = move(queue_.front());
+			queue_.pop();
+
+			return true;
+		}
+
+		size_t size()
+		{
+			lock_guard<mutex> lock(mutex_);
+			return queue_.size();
+		}
+
+		bool isEmpty()
+		{
+			lock_guard<mutex> lock(mutex_);
+			return queue_.empty();
+		}
+
+		void clear()
+		{
+			lock_guard<mutex> lock(mutex_);
+			while (!queue_.empty())
+				queue_.pop();
+		}
+
+	private:
+		queue<T> queue_;
+		mutex mutex_;
+	};
+
+	class Pipeline
+	{
+	public:
+		Pipeline(Pipeline const&) = delete;
+		Pipeline(Pipeline&&) = delete;
+		Pipeline& operator=(Pipeline const&) = delete;
+		Pipeline& operator=(Pipeline&&) = delete;
+
+		Pipeline()
+		{}
+
+		virtual ~Pipeline()
+		{
+			deque_.clear();
+		}
+
+		size_t push(uint8_t _in)
+		{
+			lock_guard<mutex> lock(mutex_);
+			deque_.push_back(_in);
+
+			return deque_.size();
+		}
+
+		template <typename T> 
+		size_t push(const T& _in)
+		{
+			uint8_t* add = (uint8_t*)(&_in);
+
+			lock_guard<mutex> lock(mutex_);
+			deque_.resize(deque_.size() + sizeof(T));
+			copy_backward(add, add+sizeof(T), deque_.end());
+
+			return sizeof(T);
+		}
+
+		size_t push(const char* _buf, int _len)
+		{
+			lock_guard<mutex> lock(mutex_);
+
+			deque_.resize(deque_.size() + _len);
+			copy_backward(_buf, _buf + _len, deque_.end());
+
+			return static_cast<size_t>(_len);
+		}
+
+		template <typename T> 
+		size_t push(const vector<T>& _vector)
+		{
+			size_t pushCount{ 0 };
+
+			lock_guard<mutex> lock(mutex_);
+			for (auto it: _vector)
+			{
+				uint8_t* add = (uint8_t*)(&it);
+
+				deque_.resize(deque_.size() + sizeof(T));
+				copy_backward(add, add+sizeof(T), deque_.end());
+
+				pushCount += sizeof(T);
+			}
+				
+			return pushCount;
+		}
+
+		size_t push(const string& _str)
+		{
+			lock_guard<mutex> lock(mutex_);
+
+			size_t strLen = _str.length();
+
+			deque_.resize(deque_.size() + strLen);
+			copy_backward(_str.begin(), _str.end(), deque_.end());
+
+			return strLen;
+		}
+
+		template <typename T>  const deque<uint8_t>::iterator search(const T& _in)
+		{
+			uint8_t* add = (uint8_t*)(&_in);
+
+			deque<uint8_t> temp;
+			temp.resize(sizeof(T));
+			copy_backward(add, add+sizeof(T), temp.end());
+
+			deque<uint8_t>::iterator  it(deque_.end());
+
+			it = std::search(deque_.begin(), deque_.end(), temp.begin(), temp.end());
+
+			return it;
+		}
+
+		const deque<uint8_t>::iterator search(const char* _buf, const int _len)
+		{
+			deque<uint8_t> temp;
+			temp.resize(_len);
+			copy_backward(_buf, _buf+_len, temp.end());
+
+			deque<uint8_t>::iterator  it(deque_.end());
+
+			it = std::search(deque_.begin(), deque_.end(), temp.begin(), temp.end());
+
+			return it;
+		}
+
+
+		const deque<uint8_t>::iterator search(const string& _str)
+		{
+			deque<uint8_t> temp;
+			temp.resize(_str.length());
+			copy_backward(_str.begin(), _str.end(), temp.end());
+
+			deque<uint8_t>::iterator  it = std::search(deque_.begin(), deque_.end(), temp.begin(), temp.end());
+
+			return it;
+		}
+
+		long long at(deque<uint8_t>::iterator _it)
+		{
+			if (_it != deque_.end())
+				return std::distance(deque_.begin(), _it);
+
+			return -1;
+		}
+
+		size_t pop(char* _buf, int _len)
+		{
+			lock_guard<mutex> lock(mutex_);
+
+			size_t dSize(deque_.size());
+
+			if (_len >= static_cast<int>(dSize))
+			{
+				copy(deque_.begin(), deque_.end(), _buf);
+				deque_.erase(deque_.begin(), deque_.end());
+			}
+			else
+			{
+				copy(deque_.begin(), deque_.begin() + _len, _buf);
+				deque_.erase(deque_.begin(), deque_.begin() + _len);
+
+				dSize = _len;
+			}
+
+			return dSize;
+		}
+
+		size_t isEmpty() const
+		{
+			lock_guard<mutex> lock(mutex_);
+			return deque_.empty();
+		}
+
+		void clear()
+		{
+			lock_guard<mutex> lock(mutex_);
+			deque_.clear();
+		}
+
+		size_t length() const
+		{
+			lock_guard<mutex> lock(mutex_);
+			return deque_.size();
+		}
+
+		friend ostream& operator<<(ostream& _os, Pipeline& _pipeline)
+		{
+			ios_base::fmtflags f{ _os.flags() };
+
+			for (auto i:_pipeline.deque_)
+			{
+				_os << right << setw(2) << setfill('0') << hex << +(i) << ' ';
+			}
+
+			_os.flags(f);
+			_os << endl;
+
+			return _os;
+		}
+
+	private:
+		deque<uint8_t> deque_;
+		mutable mutex mutex_;
+	};
+}
